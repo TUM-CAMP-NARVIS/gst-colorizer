7aa00368dacfd4640adf63873a8f0ba306ec521e
Ulrich Eck <ulrich.eck@tum.de>
2020-04-26 12:28:45 +0200

    adapted to camp conan

--- conanfile.py
+++ conanfile.py
@@ -3,22 +3,23 @@ import os
 
 class GstreamerColorizerConan(ConanFile):
     name = "gstreamer-colorizer"
+    version = "0.1"
     license = "LGPL"
     description = "Plugin to colorize 16 bit grayscale depth images with a color map"
     url = "https://aivero.com"
     settings = "os", "arch", "compiler", "build_type"
     exports_sources = ["CMakeLists.txt", "src/*"]
 
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        tag, branch = git.get_tag(), git.get_branch()
-        self.version = tag if tag and branch.startswith("HEAD") else branch
+    # def set_version(self):
+    #     git = tools.Git(folder=self.recipe_folder)
+    #     tag, branch = git.get_tag(), git.get_branch()
+    #     self.version = tag if tag and branch.startswith("HEAD") else branch
 
-    def build_requirements(self):
-        self.build_requires("cmake/[>=3.15.3]@%s/stable" % (self.user))
+    # def build_requirements(self):
+    #     self.build_requires("cmake/[>=3.15.3]@%s/stable" % (self.user))
 
     def requirements(self):
-        gst_version = "master" if self.version == "master" else "[~%s]" % "1.16.0"
+        gst_version = "master" if self.version == "master" else "[~%s]" % "1.16.2"
         gst_channel = "testing" if self.version == "master" else "stable"
 
         self.requires("gstreamer-plugins-base/%s@%s/%s" % (gst_version, self.user, gst_channel))
